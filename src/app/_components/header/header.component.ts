50ee716affa5cb5b45fe2fe900ac5d6f923ce6f0
vanaheimex <thegreatlichking@protonmail.com>
2021-04-30 11:57:46 -0700

    pooled rune for chaosnet

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, SimpleChange } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
 import { User } from 'src/app/_classes/user';
+import { MidgardService } from 'src/app/_services/midgard.service';
 import { MainViewsEnum, OverlaysService } from 'src/app/_services/overlays.service';
 import { PhraseConfirmService } from 'src/app/_services/phrase-confirm.service';
 import { UserService } from 'src/app/_services/user.service';
 import { environment } from 'src/environments/environment';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [DecimalPipe]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
 
@@ -19,6 +22,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
   subs: Subscription[];
   //uses for hiding in confirmation view
   isUnderstood: boolean;
+  topbar: string;
+  totalPooledRune: number;
+  maxLiquidityRune: number;
+  depositsDisabled: boolean;
 
   private _overlay: boolean;
   get overlay(): boolean {
@@ -30,7 +37,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
   @Output() overlayChange = new EventEmitter<boolean>();
 
-  constructor(private userService: UserService, public overlaysService: OverlaysService, private phraseConfirm: PhraseConfirmService) {
+  constructor(private userService: UserService, public overlaysService: OverlaysService, private phraseConfirm: PhraseConfirmService, private midgardService: MidgardService, private _decimalPipe: DecimalPipe) {
     this.isTestnet = environment.network === 'testnet' ? true : false;
 
     const user$ = this.userService.user$.subscribe(
@@ -48,11 +55,34 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.getPoolCap();
   }
 
   gotoSwap() {
     if (this.isUnderstood)
       this.overlaysService.setViews(MainViewsEnum.Swap, 'Swap');
+  }
+
+  getPoolCap() {
+    const mimir$ = this.midgardService.getMimir();
+    const network$ = this.midgardService.getNetwork();
+    const combined = combineLatest([mimir$, network$]);
+
+    this.topbar = "LOADING CAPS";
+    const sub = combined.subscribe( ([mimir, network]) => {
+
+      this.totalPooledRune = +network.totalPooledRune / (10 ** 8);
+
+      if (mimir && mimir['mimir//MAXIMUMLIQUIDITYRUNE']) {
+        this.maxLiquidityRune = mimir['mimir//MAXIMUMLIQUIDITYRUNE'] / (10 ** 8);
+        this.depositsDisabled = (this.totalPooledRune / this.maxLiquidityRune >= .9);
+
+        this.topbar = `${this._decimalPipe.transform(this.totalPooledRune, '0.0-0')} / ${this._decimalPipe.transform(this.maxLiquidityRune, '0.0-0')} RUNE POOLED`
+      }
+
+    });
+
+    this.subs.push(sub);
   }
 
   ngOnChanges(changes: SimpleChange) {
