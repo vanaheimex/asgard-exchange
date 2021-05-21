d2cf439dd749ac8d9b669d8c16ea8b94ce3ee4ce
vanaheimex <thegreatlichking@protonmail.com>
2021-05-21 11:35:08 -0700

    slippage setting will remeber the values

---
+++ src/app/_services/swap-service.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import BigNumber from 'bignumber.js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SwapServiceService {
+  private targetAmount: BigNumber = new BigNumber(0);
+  private sourceAmount: number = 0;
+  private sourceAsset;
+  private targetAsset;
+
+  setTarget(val: BigNumber) {
+    this.targetAmount = val;
+  }
+
+  setSource(val: number) {
+    this.sourceAmount = val;
+  }
+
+  getTargetAmount() {
+    return this.targetAmount;
+  }
+
+  getSourceAmount() {
+    return this.sourceAmount;
+  }
+}
